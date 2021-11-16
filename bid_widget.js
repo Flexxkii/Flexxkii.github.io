ed8ef2ccd42d7f8591a80efe59f4339698989cfa
Teodor Anthony <tedytorres112@gmail.com>
2021-11-16 12:27:07 +0100

    Update bid_widget.js

--- bid_widget.js
+++ bid_widget.js
@@ -780,7 +780,7 @@ function load_widget() {
             fetch_agency.results.forEach(function (value, index) {
                 var this_id = value._id;
                 var this_btn_color = value.button_color;
-                var this_color = value.Color;
+                var this_color = value.Color';
                 var this_info = value.bids_information;
                 
                 console.log("Agency ID: " + this_id);
@@ -788,8 +788,8 @@ function load_widget() {
                 
                 // if agency id equals object agency id
                 if(this_id == agency_obj_id) {
-                    object_color = this_color;
-                    object_btn_color = this_btn_color;
+                    object_color = this_color || '#ff9a02';
+                    object_btn_color = this_btn_color || '#ff9a02';
                     bid_info = this_info || 'Er is geen descriptie tekst';
                     
                     // appends stylesheet
