27535f7e89e6c3e96a4d47248a7541b930e2054b
Teodor Anthony <tedytorres112@gmail.com>
2021-11-16 11:34:29 +0100

    Update bid_widget.js

--- bid_widget.js
+++ bid_widget.js
@@ -5,7 +5,7 @@ var currentpath_split = currentpath.split('/');
 var currentpath_last = currentpath_split[currentpath_split.length - 1];
 
 // Bieding website link
-var sw24_api = 'https://www.streetwise24.com/version-test/customer_dashboard';
+var sw24_api;
 
 // Variables
 var window_button;
@@ -18,6 +18,9 @@ var object_btn_color;
 var fetch_obj;
 var fetch_agency;
 var bgcolor;
+var agency;
+var bid_info;
+var bid_info_formatted;
 
 //CSS variables for dark mode
 function activateDarkMode(object_color_var, object_btn_color_var) {
@@ -33,7 +36,8 @@ function activateDarkMode(object_color_var, object_btn_color_var) {
     '--bid-btn-boxshadow': '0px 10px 10px 0 rgba(0,0,0,0.25)',
     '--bid-btn-boxshadow-hover': '0px 15px 15px 0' + hexToRgbA(object_btn_color_var),
     '--venster-btn-boxshadow': '0px 10px 10px 0 rgba(0,0,0,0.25)',
-    '--tooltip-bgcolor': '#1F252C transparent transparent transparent'
+    '--tooltip-bgcolor': '#1F252C transparent transparent transparent',
+    '--tooltip-color': '#FFFFFF'
   }
   for(k in darkTheme) {
     rootElement.style.setProperty(k, darkTheme[k])
@@ -41,20 +45,21 @@ function activateDarkMode(object_color_var, object_btn_color_var) {
 }
 
 //CSS variables for light mode
-function activateLightMode(object_color_var, object_btn_color_var) {
+function activateLightMode(object_color_var, object_btn_color_var, fontcolor3) {
   const rootElement = document.querySelector(':root')
   const lightTheme = {
     '--background-gradient-1': '#EFEFEF',
     '--background-gradient-2': '#FFFFFF',
     '--label-color': '#111111',
-    '--value-color': object_color_var,
+    '--value-color': fontcolor3,
     '--help-btn-background': '#e1e1e1',
     '--help-btn-background-hover': '#cbcbcb',
     '--help-btn-color': '#333333',
     '--bid-btn-boxshadow': '0px 10px 10px 0 ' + hexToRgbA(object_btn_color_var),
     '--bid-btn-boxshadow-hover': '0px 15px 15px 0 ' + hexToRgbA(object_btn_color_var),
     '--venster-btn-boxshadow': '0px 10px 10px 0 ' + hexToRgbA(object_color_var),
-    '--tooltip-bgcolor': '#EFEFEF transparent transparent transparent'
+    '--tooltip-bgcolor': '#EFEFEF transparent transparent transparent',
+    '--tooltip-color': '#181d23'
   }
   for(k in lightTheme) {
     rootElement.style.setProperty(k, lightTheme[k])
@@ -64,15 +69,38 @@ function activateLightMode(object_color_var, object_btn_color_var) {
 // Sets a color scheme for the website.
 // If browser supports "prefers-color-scheme" it will respect the setting for light or dark mode
 // otherwise it will set a dark theme during night time
-function setColorScheme(object_color_var, object_btn_color_var) {
-    console.log('setColorScheme()');
-  const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches
-  const isLightMode = window.matchMedia("(prefers-color-scheme: light)").matches
-  const isNotSpecified = window.matchMedia("(prefers-color-scheme: no-preference)").matches
-  const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified;
+function setColorScheme() {
+    const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches
+    const isLightMode = window.matchMedia("(prefers-color-scheme: light)").matches
+    const isNotSpecified = window.matchMedia("(prefers-color-scheme: no-preference)").matches
+    const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified;
+    
+    var contrastRatio = 2.5;
+    
+    const color1rgb = hexToRgb('#FFFFFF');
+    const color2rgb = hexToRgb(object_btn_color);
+    const color3rgb = hexToRgb(object_color);
+                    
+    const color1luminance = luminance(color1rgb.r, color1rgb.g, color1rgb.b);
+    const color2luminance = luminance(color2rgb.r, color2rgb.g, color2rgb.b);
+    const color3luminance = luminance(color3rgb.r, color3rgb.g, color3rgb.b);
+                    
+    const ratio = color1luminance > color2luminance 
+        ? ((color2luminance + 0.05) / (color1luminance + 0.05))
+        : ((color1luminance + 0.05) / (color2luminance + 0.05));
+        
+    const ratio2 = color1luminance > color3luminance 
+        ? ((color3luminance + 0.05) / (color1luminance + 0.05))
+        : ((color1luminance + 0.05) / (color3luminance + 0.05));
+        
+    var fontcolor = ratio < 1/contrastRatio ? '#FFFFFF' : '#181d23';
+    var fontcolor2 = ratio2 < 1/contrastRatio ? '#FFFFFF' : '#181d23';
+    var fontcolor3 = ratio2 < 1/contrastRatio ? object_color : '#181d23';
+    
+    console.log(fontcolor3);
 
-  window.matchMedia("(prefers-color-scheme: dark)").addListener(e => e.matches && activateDarkMode(object_color_var, object_btn_color_var))
-  window.matchMedia("(prefers-color-scheme: light)").addListener(e => e.matches && activateLightMode(object_color_var, object_btn_color_var))
+    window.matchMedia("(prefers-color-scheme: dark)").addListener(e => e.matches && activateDarkMode(object_color, object_btn_color))
+    window.matchMedia("(prefers-color-scheme: light)").addListener(e => e.matches && activateLightMode(object_color, object_btn_color, fontcolor3))
 
 //   if(isDarkMode) activateDarkMode(object_color_var, object_btn_color_var)
 //   if(isLightMode) activateLightMode(object_color_var, object_btn_color_var)
@@ -89,12 +117,23 @@ function setColorScheme(object_color_var, object_btn_color_var) {
     var hour = now.getHours();
     
     // if it's evening use dark mode
-    if (hour < 9 || hour >= 22) {
-        activateDarkMode(object_color_var, object_btn_color_var);
+    if (hour < 9 || hour >= 20) {
+        activateDarkMode(object_color, object_btn_color);
     } else {
-        activateLightMode(object_color_var, object_btn_color_var)
+        activateLightMode(object_color, object_btn_color, fontcolor3)
     }
-  document.head.insertAdjacentHTML("beforeend", setStyling(object_color_var, object_btn_color_var));
+  document.head.insertAdjacentHTML("beforeend", setStyling(object_color, object_btn_color, fontcolor, fontcolor2));
+}
+
+// Get luminance from RGB
+function luminance(r, g, b) {
+    var a = [r, g, b].map(function (v) {
+        v /= 255;
+        return v <= 0.03928
+            ? v / 12.92
+            : Math.pow( (v + 0.055) / 1.055, 2.4 );
+    });
+    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
 }
 
 // converts HEX to HSL
@@ -166,8 +205,23 @@ function hexToRgbA(hex){
     throw new Error('Bad Hex');
 }
 
+// function to convert HEX to RGB
+function hexToRgb(hex) {
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
 // function to append styling to head
-function setStyling(setcolor, bgcolor) {
+function setStyling(setcolor, bgcolor, fontcolor, fontcolor2) {
     return `
     <style class="bieding_widget_stylesheet">
         .tekst-title {color:yellow;}
@@ -272,7 +326,7 @@ function setStyling(setcolor, bgcolor) {
         .bied_venster_btn svg {
             transform: scale(1.5);
             transition: transform .5s ease;
-            fill: white!important;
+            fill: ${fontcolor2}!important;
         }
         
         .bied_venster_btn:hover .bied_venster_btn_svg {transform: scale(1.75);}
@@ -283,7 +337,7 @@ function setStyling(setcolor, bgcolor) {
             font-size: 11pt;
             font-weight: 400;
             background: linear-gradient(0deg, var(--background-gradient-1), var(--background-gradient-2));
-            color: #fff;
+            color: var(--tooltip-color);
             text-align: center;
             border-radius: 6px;
             padding: 5px 0;
@@ -377,7 +431,7 @@ function setStyling(setcolor, bgcolor) {
         }
         
         .bied_action_bid {
-            color: white;
+            color: ${fontcolor};
             background: ${getColor(bgcolor)};
             box-shadow: var(--bid-btn-boxshadow);
             transition: all .5s ease;
@@ -399,7 +453,7 @@ function setStyling(setcolor, bgcolor) {
         .bied_action_bid:hover {
             background: ${getLighterColor(bgcolor)};
             box-shadow: var(--bid-btn-boxshadow-hover);
-            color: white;
+            color: ${fontcolor};
             text-decoration: none;
             transform: translateY(-5px);
             transition: all 1s ease;
@@ -414,16 +468,22 @@ function setStyling(setcolor, bgcolor) {
         .bied_detail {
             grid-column: 1 / 3;
             grid-row: 2;
-            background: rgba(0,0,0,0.15);
+            background: rgba(0,0,0,0.05);
             color: white;
             padding: 20px;
             border-radius: 20px;
             height: 0;
             opacity:0;
+            overflow:scroll;
             transition: all 1s ease;
         }
         
         .bied_balk.expand_window .bied_detail {opacity:1;height: 350px;}
+        
+        .bied_detail span {
+            color: var(--label-color);
+            white-space: pre-wrap;
+        }
         
         
         
@@ -449,12 +509,14 @@ function setStyling(setcolor, bgcolor) {
                 gap: 0;
                 flex-direction: column;
                 justify-content: space-between;
-                padding-bottom: 20px;
+                padding: 0 20px 20px 20px;
+                align-items: start;
             }
             
             .bied_datum {
                 flex-direction: column;
                 gap:0px;
+                margin-right: 0;
             }
             
             .bied_startprijs_container {
@@ -527,6 +589,15 @@ function setStyling(setcolor, bgcolor) {
             
             .bied_container {
                 padding: 0 10px;
+                grid-template-columns: 100px auto auto;
+            }
+            
+            .bied_info {
+                grid-column: 2;
+                flex-direction: row;
+                padding: 0;
+                align-items: flex-start;
+                gap: 20px;
             }
             
             .bied_datum {
@@ -538,7 +609,30 @@ function setStyling(setcolor, bgcolor) {
             .bied_action {
                 flex-direction: column;
                 justify-content: center;
+                align-self: start;
                 gap: 5px;
+                grid-column: 3;
+                margin-top: 0;
+            }
+            
+            .bied_venster_btn {
+                margin-top: -215px;
+                animation-name: fadeInAnimationTablet;
+            }
+            
+            .bied_balk.expand_window .bied_venster_btn { margin-bottom: 20px; }
+            
+            .bied_balk.expand_window .bied_detail { height: 430px; }
+            
+            @keyframes fadeInAnimationTablet {
+                from {
+                    margin-top: -215px;
+                    opacity: 0;
+                }
+                to {
+                    margin-top: -190px;
+                    opacity: 1;
+                }
             }
         }      
         
@@ -594,11 +688,11 @@ function setStyling(setcolor, bgcolor) {
 }
 
 // Widget HTML object
-var object_item = ( sw24_url, waarde, datum_start, datum_end ) => `
+var object_item = ( sw24_url, waarde, datum_start, datum_end, this_info) => `
 <div class="bied_balk">
     <div class="bied_container">
         <div class="bied_venster_btn">
-            <span class="bid_tooltiptext">Schuif naar beneden</span>
+            <span class="bid_tooltiptext">Schuif venster</span>
             <svg class="bied_venster_btn_svg" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30" style=" fill:#000000;">    <path d="M 15.980469 1.9902344 A 1.0001 1.0001 0 0 0 15.292969 2.2929688 L 9.2929688 8.2929688 A 1.0001 1.0001 0 1 0 10.707031 9.7070312 L 16.707031 3.7070312 A 1.0001 1.0001 0 0 0 15.980469 1.9902344 z M 18 5 A 1 1 0 0 0 17.287109 5.2988281 L 12.328125 10.261719 A 1 1 0 0 0 12 11 A 1 1 0 0 0 12.355469 11.765625 L 18.285156 17.699219 A 1 1 0 0 0 19 18 A 1 1 0 0 0 19.708984 17.705078 L 24.697266 12.716797 A 1 1 0 0 0 25 12 A 1 1 0 0 0 24.701172 11.287109 L 18.716797 5.3027344 A 1 1 0 0 0 18.705078 5.2910156 A 1 1 0 0 0 18 5 z M 26.980469 12.990234 A 1.0001 1.0001 0 0 0 26.292969 13.292969 L 20.292969 19.292969 A 1.0001 1.0001 0 1 0 21.707031 20.707031 L 27.707031 14.707031 A 1.0001 1.0001 0 0 0 26.980469 12.990234 z M 13.292969 15.292969 L 3.7714844 23.423828 A 2 2 0 0 0 3 25 A 2 2 0 0 0 5 27 A 2 2 0 0 0 6.5546875 26.257812 A 2 2 0 0 0 6.5644531 26.246094 L 6.5664062 26.244141 L 14.707031 16.707031 L 13.292969 15.292969 z"></path></svg>
         </div>
         <div class="bied_info">
@@ -637,29 +731,30 @@ var object_item = ( sw24_url, waarde, datum_start, datum_end ) => `
                 <span class="bid_tooltiptext">Toon info</span>
                 <span>info</span>
             </a>
-            <a class="bied_action_bid" target="_blank" href="${sw24_url}">
+            <a class="bied_action_bid" target="_blank" href="https://www.streetwise24.com/version-test/create__account_customer?unique_id=${sw24_url}">
                 <span class="bid_tooltiptext">Ga naar bieding</span>
                 <span>Bied</span>
             </a>
         </div>
         <div class="bied_detail">
-            <span>Detail window</span>
+            <span>${this_info}</span>
         </div>
     </div>
 </div>
 `;
 
-// Widget loading function
+// Widget API & appending loading function
 function load_widget() {
     // API request to Properties
     fetch('https://www.streetwise24.com/version-test/api/1.1/obj/properties').then(function(response) {
         return response.json();
     }).then(function(obj) {
         fetch_obj = obj.response.results;
+        console.log(fetch_obj);
 
         // Loop each results
         fetch_obj.forEach(function (value, index) {
-            var agency = value.Agency;
+            agency = value.Agency;
             var house_id = value.house_id;
             
             // if house id exist
@@ -679,20 +774,25 @@ function load_widget() {
             return response.json();
         }).then(function(agency_obj) {
             fetch_agency = agency_obj.response;
+            console.log(fetch_agency);
             
             // Loop each results
             fetch_agency.results.forEach(function (value, index) {
                 var this_id = value._id;
                 var this_btn_color = value.button_color;
                 var this_color = value.Color;
+                var this_info = value.bids_information;
+                
+                console.log(this_id);
                 
                 // if agency id equals object agency id
                 if(this_id == agency_obj_id) {
                     object_color = this_color;
                     object_btn_color = this_btn_color;
+                    bid_info = this_info || 'Er is geen descriptie tekst';
                     
                     // appends stylesheet
-                    setColorScheme(this_color, this_btn_color);
+                    setColorScheme();
                     
                 }
             }); 
@@ -710,20 +810,16 @@ function load_widget() {
                 var house_date_start_formatted = `${house_date_start.getDate()}/${house_date_start.getMonth()}/${house_date_start.getFullYear()}`;
                 var house_date_end = new Date(value.end_date_bid);
                 var house_date_end_formatted = `${house_date_end.getDate()}/${house_date_end.getMonth()}/${house_date_end.getFullYear()}`;
-                var enable_bidding = value.Bids_property;
+                var enable_bidding = value.bids_property;
                 
                 // if house_id exist
                 if(typeof house_id !== 'undefined') {
+                    console.log(value);
                     // if house_id equals URL house id
-                    if(house_id == currentpath_last) {
-                        console.log(house_date_start_formatted);
-                        console.log(house_date_end_formatted);
-                        console.log(enable_bidding);
-                        console.log(value.start_date_bid);
-                        console.log(value.end_date_bid);
+                    if(house_id == currentpath_last && enable_bidding) {
                         
                         // appends widget object to body
-                        document.body.insertAdjacentHTML("beforeend", object_item(sw24_api, start_price_formatted, house_date_start_formatted, house_date_end_formatted));
+                        document.body.insertAdjacentHTML("beforeend", object_item(agency, start_price_formatted, house_date_start_formatted, house_date_end_formatted, bid_info));
                     }
                 }
             }); 
