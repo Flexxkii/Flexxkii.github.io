3ca96f676f60b54fb07b70d0a3d1f3abe5bd8f16
Teodor Anthony <tedytorres112@gmail.com>
2022-01-04 15:34:16 +0100

    Create smartproperty_script.js

---
+++ smartproperty_script.js
@@ -0,0 +1,11 @@
+var block_1 = $('#innergrid_2cf');
+var block_2 = $('#innergrid_pe7');
+var logo_block = $('#1065921513');
+
+block_1.on('hover', function() {
+	logo_block.attr('src', 'https://irp.cdn-website.com/5ec89aa2/dms3rep/multi/SP+Makelaardij+Logo.svg');
+});
+
+block_2.on('hover', function() {
+	logo_block.attr('src', 'https://irp.cdn-website.com/5ec89aa2/dms3rep/multi/SP+Projecten+Logo.svg');
+});
